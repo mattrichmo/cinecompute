ac7c393ec1b2b337598a0d264bc49a294a60f292
mattrichmo <matt.richmond3@gmail.com>
2024-06-08 15:29:27 -0700

    added types

--- src/app/components/InfoCard/InfoCard.tsx
+++ src/app/components/InfoCard/InfoCard.tsx
@@ -1,33 +1,38 @@
 // infocard.tsx
 
-const InfoCard = ({ title, value }) => {
-    return (
-        <div className="absolute top-0 right-0 m-10 z-30 max-w-xs cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
-        <img className="w-full rounded-lg object-cover object-center" src="./img/default.webp" alt="product" />
-        <div>
-          <div className="my-6 flex items-center justify-between px-4">
-            <p className="font-bold text-gray-500">Name</p>
-            <p className="rounded-full bg-blue-500 px-2 py-0.5 text-xs font-semibold text-white">{title}</p>
-          </div>
-          <div className="my-4 flex items-center justify-between px-4">
-            <p className="text-sm font-semibold text-gray-500">Number of Connections</p>
-            <p className="rounded-full bg-gray-200 px-2 py-0.5 text-xs font-semibold text-gray-600">99</p>
-          </div>
-          <div className="my-4 flex items-center justify-between px-4">
-            <p className="text-sm font-semibold text-gray-500">Number of Films</p>
-            <p className="rounded-full bg-gray-200 px-2 py-0.5 text-xs font-semibold text-gray-600">{value}</p>
-          </div>
-          <div className="my-4 flex items-center justify-between px-4">
-            <p className="text-sm font-semibold text-gray-500">Third option</p>
-            <p className="rounded-full bg-gray-200 px-2 py-0.5 text-xs font-semibold text-gray-600">1</p>
-          </div>
-          <div className="my-4 flex items-center justify-between px-4">
-            <p className="text-sm font-semibold text-gray-500">Fourth option</p>
-            <p className="rounded-full bg-gray-200 px-2 py-0.5 text-xs font-semibold text-gray-600">23</p>
-          </div>
-        </div>
-      </div>
-    )
+interface InfoCardProps {
+  title: string;
+  value: number;
 }
 
-export default InfoCard
+const InfoCard = ({ title, value }: InfoCardProps) => {
+  return (
+    <div className="absolute top-0 right-0 m-10 z-30 max-w-xs cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
+      <img className="w-full rounded-lg object-cover object-center" src="./img/default.webp" alt="product" />
+      <div>
+        <div className="my-6 flex items-center justify-between px-4">
+          <p className="font-bold text-gray-500">Name</p>
+          <p className="rounded-full bg-blue-500 px-2 py-0.5 text-xs font-semibold text-white">{title}</p>
+        </div>
+        <div className="my-4 flex items-center justify-between px-4">
+          <p className="text-sm font-semibold text-gray-500">Number of Connections</p>
+          <p className="rounded-full bg-gray-200 px-2 py-0.5 text-xs font-semibold text-gray-600">99</p>
+        </div>
+        <div className="my-4 flex items-center justify-between px-4">
+          <p className="text-sm font-semibold text-gray-500">Number of Films</p>
+          <p className="rounded-full bg-gray-200 px-2 py-0.5 text-xs font-semibold text-gray-600">{value}</p>
+        </div>
+        <div className="my-4 flex items-center justify-between px-4">
+          <p className="text-sm font-semibold text-gray-500">Third option</p>
+          <p className="rounded-full bg-gray-200 px-2 py-0.5 text-xs font-semibold text-gray-600">1</p>
+        </div>
+        <div className="my-4 flex items-center justify-between px-4">
+          <p className="text-sm font-semibold text-gray-500">Fourth option</p>
+          <p className="rounded-full bg-gray-200 px-2 py-0.5 text-xs font-semibold text-gray-600">23</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoCard;
