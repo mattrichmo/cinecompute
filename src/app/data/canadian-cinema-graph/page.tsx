ac627e4e4dc2ee1a98bae2328103c452c5a09c32
mattrichmo <matt.richmond3@gmail.com>
2024-06-09 10:19:24 -0700

    changed the forcegraph to use raw json data instead of an api call

--- src/app/data/canadian-cinema-graph/page.tsx
+++ src/app/data/canadian-cinema-graph/page.tsx
@@ -6,12 +6,11 @@
 import React from 'react';
 // Import server components with .server.js extensions
 import ForceGraph from "@/app/components/ForceGraph/ForceGraph";
-import getProducerData from '@/app/components/utilities/getProducerData/getProducerData';
+import data from "../../../../public/data/datasets/producers.json"
 
 // Convert to default server component export
 export default async function ProducerGraph() {
   // Fetch data asynchronously from the API route
-  let data = await getProducerData();  
   return (
     <div style={{ backgroundColor: 'rgba(0, 0, 17, 0.5)' }} className="relative min-h-screen text-white">
       <div className="absolute top-0 left-0 w-full h-full z-10">
