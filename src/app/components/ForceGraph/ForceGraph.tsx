010609d569e14e551a1155ef85b8792fb8348c96
mattrichmo <matt.richmond3@gmail.com>
2024-06-09 11:24:54 -0700

    fixes

--- src/app/components/ForceGraph/ForceGraph.tsx
+++ src/app/components/ForceGraph/ForceGraph.tsx
@@ -1,59 +1,23 @@
+
+
+
 "use client";
 
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ForceGraph3D } from 'react-force-graph';
-import InfoCard from '@/app/components/InfoCard/InfoCard'; // Ensure the import path is correct
+import data from "../../../../public/data/datasets/producers.json"
 
-
-interface ForceGraphNode {
-    // Define properties of a node here
-    // For example:
-    id: string;
-    type?: string; // Optional, depending on your data structure
-  }
+const ForceGraphComponent = () => {
   
-  interface ForceGraphLink {
-    // Define properties of a link here
-    // For example:
-    source: string;
-    target: string;
-  }
-  
-  interface ForceGraphData {
-    nodes: ForceGraphNode[];
-    links: ForceGraphLink[];
-  }
-  
-  const ForceGraph = ({ data }: { data: ForceGraphData }) => {
-    console.log(`Number of Nodes In Force Graph: ${data.nodes.length}`, `Number of Links: ${data.links.length}`);
-
-    const fgRef = useRef();
-    const [highlightNodes, setHighlightNodes] = useState(new Set());
-    const [highlightLinks, setHighlightLinks] = useState(new Set());
-    const [clickedNodes, setClickedNodes] = useState(new Set());
-    const [clickedLinks, setClickedLinks] = useState(new Set());
-    const [activeNode, setActiveNode] = useState(null); // State to hold the active node for InfoCard
-
-    // Define colors
-    const highlightColor = 'rgb(255,0,0)';
-    const filmColor = 'rgba(229, 93, 226, 0.8)';
-    const defaultColor = 'rgba(17, 239, 225, 0.8)';
-    const clickedColor = 'rgb(255,255,0)'; // Yellow color for clicked links
-
-    return (
-        <div style={{ position: 'relative', height: '100%', width: '100%' }}>
-            <ForceGraph3D
-                ref={fgRef}
-                graphData={data}
-                nodeColor={node => {
-                    if (highlightNodes.has(node) || clickedNodes.has(node)) {
-                        return highlightColor;
-                    }
-                    return node.type === 'film' ? filmColor : defaultColor;
-                }}
-            />
-        </div>
-    );
+  return (
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <ForceGraph3D
+        graphData={data}
+        nodeAutoColorBy="type"
+        linkDirectionalParticles={1}
+      />
+    </div>
+  );
 };
 
-export default ForceGraph;
+export default ForceGraphComponent;
