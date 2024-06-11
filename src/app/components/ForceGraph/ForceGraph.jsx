95b5435bb3875419cc8aa6311b32da090887d5f3
mattrichmo <matt.richmond3@gmail.com>
2024-06-10 21:32:00 -0700

    updated text

--- src/app/components/ForceGraph/ForceGraph.jsx
+++ src/app/components/ForceGraph/ForceGraph.jsx
@@ -206,8 +206,13 @@ const ForceGraph = () => {
       </div>
       <div className="absolute top-0 mt-20 z-20 w-1/4">
         <h1 className="text-6xl mt-20 ml-20 leading-tight">Canadian Cinema Data</h1>
-        <p className="text-sm mt-10 ml-20">This graph shows relationships between Canadian productions and the producers involved. Mapping the relationships between each producer.</p>
-        <p className="text-sm mt-2 ml-20">Sourced by scraping every movie made in Canada and then filtering it out for the last 5 years.</p>
+        <div className="text-sm mt-10 ml-20">
+    <p>This 3D graph illustrates the intricate relationships within the Canadian film industry, highlighting the connections between productions, producers, companies, cast, and grips involved. It provides a comprehensive map of these relationships, particularly focusing on producer collaborations.</p>
+</div>
+<div className="text-sm mt-2 ml-20">
+    <p>The data for this visualization was meticulously sourced by scraping information from all films produced in Canada, filtered to include productions from the past year.</p>
+</div>
+
         <div className="ml-20 mt-20">
           <div className="relative mt-20 z-20">
           <Toggles selectedToggle={selectedToggle} setSelectedToggle={setSelectedToggle} />
